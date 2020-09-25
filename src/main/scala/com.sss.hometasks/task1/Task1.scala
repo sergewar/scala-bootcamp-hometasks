8ae6bc3bb44a7c6f5771f7a6b392ef0c21f3222b
sshtubey <sshtubei@evolutiongaming.com>
2020-09-25 03:50:07 +0300

    Home task 1 add tail recursion annotation

--- src/main/scala/com.sss.hometasks/task1/Task1.scala
+++ src/main/scala/com.sss.hometasks/task1/Task1.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 // Homework. Implement functions that calculate https://en.wikipedia.org/wiki/Lowest_common_denominator and
 // https://en.wikipedia.org/wiki/Greatest_common_divisor for integers.
 
@@ -9,12 +10,13 @@ object ht1 {
 
   def lcm(a: Int, b: Int): Int = if (a == 0 && b == 0) 0 else Math.abs(a * b) / gcd(a, b)
 
+  @tailrec
   def gcd(a: Int, b: Int): Int = (a, b) match {
     case (0, b) => b
     case (a, 0) => a
     case (a, b) if a == b => a
     case (a, b) if a == 1 || b == 1 => 1
-    case (a, b) if a % 2 == 0 && b % 2 == 0 => 2 * gcd(a / 2, b / 2)
+    case (a, b) if a % 2 == 0 && b % 2 == 0 => gcd(a / 2, b / 2) * 2
     case (a, b) if a % 2 == 0 && b % 2 != 0 => gcd(a / 2, b)
     case (a, b) if a % 2 != 0 && b % 2 == 0 => gcd(a, b / 2)
     case (a, b) if a % 2 != 0 && b % 2 != 0 && a < b => gcd(b - a, a)
